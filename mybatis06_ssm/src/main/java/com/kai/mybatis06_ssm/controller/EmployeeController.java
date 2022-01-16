edd6961b55453c1f6f3224ab3a8570b1aeb58339
kai.lv <kai@163.com>
2022-01-16 16:41:17 +0800

    整合 ssm

---
+++ mybatis06_ssm/src/main/java/com/kai/mybatis06_ssm/controller/EmployeeController.java
@@ -0,0 +1,32 @@
+package com.kai.mybatis06_ssm.controller;
+
+import com.kai.mybatis06_ssm.bean.Employee;
+import com.kai.mybatis06_ssm.service.EmployeeService;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @description:
+ * @author: kai.lv
+ * @date: 2022/1/14
+ **/
+@Controller
+public class EmployeeController {
+
+    @Autowired
+    EmployeeService employeeService;
+
+    @RequestMapping("/emps")
+    public String emps(Map<String,Object> map) {
+        List<Employee> emps = employeeService.getEmps();
+        map.put("allEmps", emps);
+        return "list";
+    }
+
+
+}
