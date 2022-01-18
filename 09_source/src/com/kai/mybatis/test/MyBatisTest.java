6728889f28e520efe98101d5d1a59d7330c0b878
kai.lv <kai@163.com>
2022-01-18 11:33:17 +0800

    mybatis 源码运行流程

--- 09_source/src/com/kai/mybatis/test/MyBatisTest.java
+++ 09_source/src/com/kai/mybatis/test/MyBatisTest.java
@@ -29,7 +29,26 @@ public class MyBatisTest {
      *      返回sqlSession的实现类DefaultSqlSession对象，包含Executor和Configuration;
      *      这一步会创建Executor对象
      * 3.获取接口的代理对象(MapperProxy)
+     *      getMapper,使用MapperProxyFactory创建一个MapperProxy的代理对象
+     *      代理对象里面包含了，DefaultSqlSession(Executor)
      * 4.执行增删改查方法
+     *
+     * 总结：
+     *  1、根据配置文件（全局，sql映射）初始化出Configuration对象
+     *  2、创建一个DefaultSqlSession对象，它里面包含Configuration以及
+     *      Executor（根据全局配置文件中的defaultExecutorType创建出对应的Executor）
+     *  3、DefaultSQLSession.getMapper();拿到Mapper接口对应的MapperProxy；
+     *  4、MapperProxy里面有（DefaultSqlSession）；
+     *  5、执行增删改查方法：
+     *          1）调动DefaultSQLSession的增删改查；
+     *          2）会创建一个StatementHandler对象。（同时也会创建出ParameterHandler和resultSetHandler)
+     *          3）调用StatementHandler预编译参数以及设置参数；
+     *              使用ParameterHandler来给sql设置参数
+     *          4）调用StatementHandler的增删改查方法；
+     *          5）ResultSetHandler封装结果
+     *   注意：
+     *       四大对象每个创建的时候都以一个interceptorChain.pluginAll(parameterHandler)
+     *
      *
      *
      * @throws IOException
