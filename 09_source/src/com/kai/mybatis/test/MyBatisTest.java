37614d32fc8b470630d676b9ca109f39b5073b96
kai.lv <kai@163.com>
2022-01-18 15:11:29 +0800

    插件原理

--- 09_source/src/com/kai/mybatis/test/MyBatisTest.java
+++ 09_source/src/com/kai/mybatis/test/MyBatisTest.java
@@ -71,4 +71,40 @@ public class MyBatisTest {
         }
     }
 
+
+    /**
+     * 插件原理
+     * 在四大对象创建的时候
+     * 1、每个创建出来的对象不是直接返回的，而是
+     * 		interceptorChain.pluginAll(parameterHandler);
+     * 2、获取到所有的Interceptor（拦截器）（插件需要实现的接口）；
+     * 		调用interceptor.plugin(target);返回target包装后的对象
+     * 3、插件机制，我们可以使用插件为目标对象创建一个代理对象；AOP（面向切面）
+     * 		我们的插件可以为四大对象创建出代理对象；
+     * 		代理对象就可以拦截到四大对象的每一个执行；
+     *
+     public Object pluginAll(Object target) {
+     for (Interceptor interceptor : interceptors) {
+     target = interceptor.plugin(target);
+     }
+     return target;
+     }
+     */
+
+
+    /**
+     * 插件编写：
+     * 1、编写Interceptor的实现类
+     * 2、使用@Interceptor注解完成插件签名
+     * 3、将写好的插件注册到全局配置文件中
+     */
+    @Test
+    public void testPlugin() {
+
+    }
+
+
+
+
+
 }
